ad8261185a6ee5483d12f5a1dcbc805928b64c2a
Brandon Soucie <brandonsoucie@rocketmortgage.com>
2023-01-03 11:36:49 -0500

    Updated fixed code

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ let golfholes = prompt("Hi, "+ name +" Would you like to play 3 or 6 holes of mi
 console.log(golfholes)
 
 let total = 0;
-let par = golfholes * 3;
+let par = golfholes * Number (3);
+console.log(par)
 
 for (let i = 0; i < golfholes; i++) {
-    let score = Number (prompt("How many putts?"));
+    let score = Number (prompt("How many putts on hole?"));
     total += score;
 }
+console.log (total)
 
-console.log ("Good game "+name+" Your total par was: 0")
-
-
-if (total =par )    
+if (total === par )    
 {alert("Good game " + name + ". Your total par was: 0");
 }
 
-else if (total <par) {alert ("Great job, "+name+"! Your total par was -(par)");}
+else if (total <par) {alert ("Great job, "+name+"! Your total par was" +(total-par)+".") } 
 
-else if (total >par) {alert ("Nice try, "+name+"...Your total par was +(par)")}
+else if (total >par) {alert ("Nice try, "+name+"...Your total par was "+(total-par)+".")}
+
 
 
 
